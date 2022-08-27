b0840ef1a460cf813ebfffe5351d24ea48be50b7
jordan Moldovan <jmoldyvan@gmail.com>
2022-08-27 03:22:05 -0700

    s

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Register() {
+
+    const [email,setEmail] = useState
+
   return (
     <form action=''>
         <input type='email' placeholder='email'/>
